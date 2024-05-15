57d8ddf3aa54c1b1a2e2bc19d956a4fa30514245
Matteo0421 <matteo.carola99@gmail.com>
2024-05-15 16:38:58 +0200

    [add]card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,21 @@
 
 @section('content')
 
-    <h1>Home-wine</h1>
+<div class="container d-flex flex-wrap justify-content-center ">
 
+    <h1 class="text-center">BEST SELLER</h1>
+
+    @foreach ($wines as $wine )
+<div class="card col-4 m-3" style="width: 18rem;">
+    <img src="{{$wine->image}}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{$wine->wine}}</h5>
+      <h6 class="card-subtitle mb-2 text-body-secondary">{{$wine->winery}}</h6>
+      <p class="card-text">{{$wine->rating_average}}</p>
+      <p class="card-text">{{$wine->rating_reviews}}</p>
+      <p class="card-text">{{$wine->location}}</p>
+    </div>
+  </div>
+@endforeach
+</div>
 @endsection
