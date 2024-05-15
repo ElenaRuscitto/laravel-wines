bbf47193f775ba621477b98d52ebc821c7599e6d
JacopoLombardi <lombajacopo@gmail.com>
2024-05-15 17:18:53 +0200

    [add] Home Style fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,26 @@
 
 @section('content')
 
-<div class="container d-flex flex-wrap justify-content-center ">
+    <div class="container">
 
-    <h1 class="text-center">BEST SELLER</h1>
+    <h1 class="text-center text-white py-5">BEST SELLER</h1>
 
-    @foreach ($wines as $wine )
-<div class="card col-4 m-3" style="width: 18rem;">
-    <img src="{{$wine->image}}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">{{$wine->wine}}</h5>
-      <h6 class="card-subtitle mb-2 text-body-secondary">{{$wine->winery}}</h6>
-      <p class="card-text">{{$wine->rating_average}}</p>
-      <p class="card-text">{{$wine->rating_reviews}}</p>
-      <p class="card-text">{{$wine->location}}</p>
+    <div class="row  row-cols-3 ">
+        @foreach ($wines as $wine )
+            <div class="card d-flex col-4 m-3" style="width: 18rem;">
+                <img src="{{$wine->image}}" class="card-img-top h-100" alt="...">
+
+                <div class="card-body">
+                    <h5 class="card-title">{{$wine->wine}}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{$wine->winery}}</h6>
+                    <p class="card-text">{{$wine->rating_average}}</p>
+                    <p class="card-text">{{$wine->rating_reviews}}</p>
+                    <p class="card-text">{{$wine->location}}</p>
+                </div>
+            </div>
+        @endforeach
     </div>
-  </div>
-@endforeach
-</div>
+
+    </div>
+
 @endsection
