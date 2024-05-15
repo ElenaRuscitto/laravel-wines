6c93cf892a67a5cf5b6a805fda966fb76fd677c5
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-05-15 15:25:02 +0200

    scaffolding

---
+++ resources/views/nuova-pagina.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.main')
+
+
+
+
+
+
+
+@section('content')
+
+    <h1>Nuova Pagina</h1>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+
+    <h1>Header</h1>
+
+    <a href="{{route('home')}}">Home</a>
+    <a href="{{route('nuova-pagina')}}">Nuova Pagina</a>
+
+</header>
